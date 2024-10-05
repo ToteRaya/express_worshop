7ae681b3f93dbbd983107b4c584d67471f5b71f7
Hector Arturo <ganchodemar.11@gmail.com>
2024-10-05 17:12:53 -0600

    User login

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -20,6 +21,35 @@ user.post("/", async(req, res, next) => {
         return res.status(500).json({code:500, message: 'ERROR: Datos incompletos...'});
     }
     
+});
+
+user.post("/login", async(req, res, next)=> {
+    const {mail, password} = req.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${mail}' AND user_password = '${password}';`;
+    const rows = await db.query(query);
+    console.log(rows);
+
+    if( mail && password ){
+        if (rows.length == 1){
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "viejolesbiano"); //contraseña
+
+            return res.status(200).json({code: 200, message: token});
+        }else{
+            return res.status(401).json({code: 401, message: "ERROR"});
+        }
+    }else{
+        return res.status(500).json({code: 500, message: "Campos incompletos"});
+    } 
+});
+
+user.get("/", async(req, res, next) => {
+    const query = "SELECT * FROM user"
+    const rows = await db.query(query);
+
+    return res.status(200).json({code: 200, message: rows});
 });
 
 module.exports = user;
