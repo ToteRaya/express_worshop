3091351ba9a014fb00a15d67d2e73bb498fb9535
Hector Arturo <ganchodemar.11@gmail.com>
2024-10-16 12:57:16 -0600

    login interface

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ const user = require('./routes/user');
 const auth = require('./middleware/auth');
 const notFound = require('./middleware/notFound');
 const hello = require('./middleware/hello');
+const cors = require('./middleware/cors');
 
+app.use(cors);
 app.use(morgan('dev')); //ayuda ver el error con el no. de protocolo
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", hello);
 app.use("/user",user);
+
 app.use(auth);
 app.use("/pokemon",pokemon);
+
 app.use(notFound);
  
 app.listen(process.env.PORT || 3000, () => {
--- routes/user.js
+++ routes/user.js
@@ -38,7 +38,7 @@ user.post("/login", async(req, res, next)=> {
 
             return res.status(200).json({code: 200, message: token});
         }else{
-            return res.status(401).json({code: 401, message: "ERROR"});
+            return res.status(200).json({code: 200, message: "ERROR"});
         }
     }else{
         return res.status(500).json({code: 500, message: "Campos incompletos"});
